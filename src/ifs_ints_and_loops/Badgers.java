cb57cbafa9a0a33cce582017c1cc8d073038ddc6
lyh2007 <league@WTS-IM-17.attlocal.net>
2018-10-06 11:23:33 -0700

    I put badger lyrics on a eclipse sheet while listening to music.

---
+++ src/ifs_ints_and_loops/Badgers.java
@@ -0,0 +1,36 @@
+package ifs_ints_and_loops;
+
+public class Badgers {
+	public static void main(String[] args) {
+		String b = "badger ";
+		String M = "Mushroom ";
+		String S = "Snake";
+		
+		for (int j = 0; j < 12; j++) {
+		for (int i = 0; i < 12; i++) {
+			System.out.print(b);
+		}
+		System.out.println(M);
+		System.out.println(M);
+		
+		
+		
+		
+		
+		
+		
+	}
+	}}
+	
+
+
+
+
+
+
+
+
+// Badger, badger, badger, badger, badger, badger, badger, badger, badger,
+// badger
+// Mushroom, mushroom, a
+// 
